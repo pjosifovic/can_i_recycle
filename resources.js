cafb4993ec02f69ae7d8b90d28a493f09165ce2c
Trish Griego <pmgriego@gmail.com>
2015-12-21 16:28:56 -0800

    Created resources js and began building the functionality of page

---
+++ resources.js
@@ -0,0 +1,23 @@
+var allShops = [];
+
+var zipCodes = [
+  "98101",
+  "98102",
+]
+// more zip codes to come. 27+++
+
+function Shop(name, address, url, info, phone, zipCode); // info is short for description
+  this.name = name;
+  this.address = address;
+  this.url = url;
+  this.info = info;
+}
+
+Shops.prototype.render = function() {
+  for(var i = 0; i < zipCodes.length; i ++) {
+
+  }
+}
+Shops.prototype.render = function () {
+  // body...
+};
