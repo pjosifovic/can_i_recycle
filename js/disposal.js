bcda2797c7492644316ff3216b9d6ce3deabbd07
David Livingstone <david@david.tl>
2015-12-22 10:16:10 -0800

    added item object

--- js/disposal.js
+++ js/disposal.js
@@ -1,5 +1,27 @@
+'use strict';
+
+var itemArray = [];
+
+var Item = function(name, parent, disposal, cityUrl) {
+  this.name = name;
+  this.parent = parent;
+  this.disposal = disposal;
+  this.cityUrl = cityUrl;
+};
+
+Item.prototype.render = function() {
+  // To come
+};
+
+var addItem = function(name, parent, disposal, cityUrl) {
+  var newItem = new Item(name, parent, disposal, cityUrl);
+  itemArray.push([name,newItem]);
+};
+
+addItem('Corrugated Cardboard','Paper','recycle','http://www.seattle.gov/util/MyServices/LookItUpWhatsAccepted/Paper/Cardboard/CardboardCorrugated/index.htm');
+
 // Array that holds Disposal keys and objects
-var disposalArray = []
+var disposalArray = [];
 
 // Constructor for Disposal function
 var Disposal = function(heading, symbol, text) {
